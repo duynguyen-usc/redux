c3809c5d72cfbec1be90f62ff04ed73bfb11c63e
Duy <duy@duys-mbp.home>
2018-10-05 17:16:31 -0700

    active book

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -3,10 +3,14 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    if (!this.props.book) {
+      return <div> Select a book to get started.</div>
+    }
+
     return (
       <div>
         <h3>Details for: </h3>
-        <div><
+        <div>{this.props.book.title}</div>
       </div>
     );
   }
--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,12 +1,11 @@
 
 // State arg is not application state,
 // only the state this reducer is reponsible for
-export default function (state = null, action) {
+export default function(state = null, action) {
   switch(action.type) {
 
-  case'BOOK_SELECTED':
+  case 'BOOK_SELECTED':
     return action.payload;
   }
-
-  return state
+  return state;
 }
