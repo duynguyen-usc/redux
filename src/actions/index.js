28833c51b309a7dc8ae3b592e0166379352d2c12
Duy <duy@duys-mbp.home>
2018-10-12 23:30:03 -0700

    api calls

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
-const API_KEY = 'dcffeb505be5f4120781ebf7d79c2a13'
+const API_KEY = 'dcffeb505be5f4120781ebf7d79c2a13';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+
+export function fetchWeather(city) {
+
+  const url = `${ROOT_URL}&q=${city},us`
+  return {
+    type: FETCH_WEATHER
+  };
+}
