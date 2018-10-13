dbda8ae8154ea37cb261e512a6396117ac04979c
Duy <duy@duys-mbp.home>
2018-10-12 23:37:13 -0700

    api calls

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const API_KEY = 'dcffeb505be5f4120781ebf7d79c2a13';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
@@ -7,7 +9,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
 
   const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url)
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
