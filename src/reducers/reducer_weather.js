d6acb281ab9fce14dc0cb1120ea65f40c05ad63c
Duy <duy@duys-mbp.home>
2018-10-16 01:28:35 -0700

    add weather_list

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,11 @@
-export default function(state = null, action) {
-  console.log('Action received', action);
-  return state;
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return [action.payload.data, ...state]; // state.concat(action.payload.data])
+    default:
+      return state;
+  }
 }
